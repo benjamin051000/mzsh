e4d790bb3acfc76c8ec03714b4e3e0550a74d10f
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-07-18 23:47:04 -0400

    run a single command with `-c`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod command;
 mod builtins;
 use std::io::Write;
+use std::env;
+use std::process::ExitCode;
 
 fn print_ps1() {
     print!("$ ");
@@ -18,10 +20,46 @@ fn get_input() -> String {
 }
 
 
-fn main() {
+fn interactive_mode() {
     loop {
         print_ps1();
         let input = get_input();
         command::process(input);
     }
 }
+
+enum Mode {
+    Interactive,
+    SingleCommand(String),
+    Error
+}
+
+
+fn parse_args() -> Mode {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() == 0 {
+        return Mode::Interactive;
+    }
+    else {
+       if args[0] == "-c" {
+            return Mode::SingleCommand(args[1..].join(" "));
+        } 
+    }
+
+    Mode::Error
+}
+
+
+fn main() -> ExitCode {
+    match parse_args() {
+        Mode::Interactive => interactive_mode(),
+        Mode::SingleCommand(cmd) => command::process(cmd),
+        Mode::Error => {
+            println!("Error: Unknown argument.");
+            return ExitCode::FAILURE;
+        }
+    }
+
+    ExitCode::SUCCESS
+}
+
